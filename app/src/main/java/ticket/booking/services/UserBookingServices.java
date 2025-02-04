76871ae5814e49076d4ffe98ef887ead75c82749
himanshuAhirwar <himanshuahirwar717@gmail.com>
2025-02-04 21:43:23 +0530

    First commit

---
+++ app/src/main/java/ticket/booking/services/UserBookingServices.java
@@ -0,0 +1,13 @@
+package ticket.booking.services;
+
+import ticket.booking.entities.User;
+
+import java.util.List;
+
+public class UserBookingServices {
+    private User user;
+
+    private List<User> userList;
+
+
+}
