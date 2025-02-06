9e455d674c7690bb787391a983b85fb94a71df32
himanshuAhirwar <himanshuahirwar717@gmail.com>
2025-02-06 20:43:23 +0530

    Third commit

--- app/src/main/java/ticket/booking/services/UserBookingServices.java
+++ app/src/main/java/ticket/booking/services/UserBookingServices.java
@@ -3,10 +3,12 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.userServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static com.fasterxml.jackson.databind.ObjectMapper.*;
 
@@ -25,4 +27,33 @@ public class UserBookingServices {
         File users = new File(USER_PATH);
         userList = objectmapper.readValue(users, new TypeReference<List<User>>() {});
     }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && userServiceUtil.checkPassword(user.getPassword(),user1.getHashedPassword())
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signup(User user1) {
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
+    public void saveUserListToFile() throws IOException {
+        File userFile = new File(USER_PATH);
+        objectmapper.writeValue(userFile,userList);
+    }
+    public void fetchBooing(){
+        user.printlTicket();
+    }
+
+    public void cancelBooking(String ticketId) {
+
+    }
+
 }
--- app/src/main/java/ticket/booking/util/userServiceUtil.java
+++ app/src/main/java/ticket/booking/util/userServiceUtil.java
@@ -1,4 +1,16 @@
 package ticket.booking.util;
 
+
+import org.mindrot.jbcrypt.BCrypt;
+
 public class userServiceUtil {
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword,BCrypt.gensalt());
+    }
+
+    public static Boolean checkPassword(String plainPassword, String hashPassword) {
+        return BCrypt.checkpw(plainPassword,hashPassword);
+    }
+
 }
